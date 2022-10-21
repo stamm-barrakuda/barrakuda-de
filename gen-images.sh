3a21ab4bd16952e1cbb38ec8e75967a9742247a4
jzbor <jzbor@posteo.net>
2022-10-21 19:12:59 +0200

    Updating gen-images to copy everything to uploades dir

--- gen-images.sh
+++ gen-images.sh
@@ -3,6 +3,7 @@
 OPACITY="50%"
 SOURCE_DIR="./images"
 OUTPUT_DIR="./static"
+UPLOAD_DIR="$OUTPUT_DIR/$SOURCE_DIR/uploads"
 
 
 die () {
@@ -11,17 +12,27 @@ die () {
 }
 
 [ -d "$OUTPUT_DIR" ] || mkdir -v "$OUTPUT_DIR"
+[ -d "$UPLOAD_DIR" ] || mkdir -v "$UPLOAD_DIR"
 
 for file in $(find "$SOURCE_DIR"); do
 	if [ -f "$file" ]; then
 		echo "$file:"
-		basename="$(basename "$file" | sed 's/\(.*\)\.\(.*\)/\1/')"
-		extension="$(basename "$file" | sed 's/\(.*\)\.\(.*\)/\2/')"
+		filename="$(basename "$file")"
 		dirname="$(dirname "$file")"
+		basename="$(echo "$filename" | sed 's/\(.*\)\.\(.*\)/\1/')"
+		extension="$(echo "$filename" | sed 's/\(.*\)\.\(.*\)/\2/')"
 
 		echo "Creating resized version"
 		resized="$OUTPUT_DIR/$file"
 		convert "$file" -resize 'x1080>' "$OUTPUT_DIR/$file" || die
+
+		echo "Adding file to uploads"
+		if [ -f "$UPLOAD_DIR/$filename" ]; then
+			echo "  File already exists"
+		else
+			upload="$UPLOAD_DIR/$filename"
+			cp "$resized" "$upload"
+		fi
 
 		if echo "$dirname" | grep '/headers$' > /dev/null; then
 			echo "Creating header versions"
