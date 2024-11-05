6501e038f48bd0bd3dde80d4d0be2d80f919e908
jzbor <jzbor@posteo.net>
2024-11-05 14:17:37 +0100

    Updating shell.nix

--- shell.nix
+++ shell.nix
@@ -3,15 +3,9 @@
   pkgs.mkShell {
     # build tools
     nativeBuildInputs = with pkgs; [
-      netlify-cli
-      git-lfs
       hugo
     ];
 
     # libraries/dependencies
     buildInputs = with pkgs; [];
-
-    shellHook = ''
-      . ~/.config/netlify/helper/path.bash.inc
-    '';
 }
